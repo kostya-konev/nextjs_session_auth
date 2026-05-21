db5e0def5c49c77e775eb4582689b055d0b6d50c
Kostya Konev <kosgoh.214@gmail.com>
2026-05-22 01:59:58 +0300

    initial part

---
+++ src/shared/providers/MainProvider.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import { type PropsWithChildren } from 'react'
+
+import { TanstackQueryProvider } from '@/shared/providers/TanstackQueryProvider'
+
+export function MainProvider({ children }: PropsWithChildren<unknown>) {
+	return <TanstackQueryProvider>{children}</TanstackQueryProvider>
+}
